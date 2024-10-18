b2114d424c1cd7ef36dbfefbf86282fecce4572e
Marci Prescott <mjprescott@me.com>
2024-10-18 17:19:53 -0600

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+var publicPath = path.resolve( __dirname,  'public');
+app.use(express.static(publicPath));
+
+
+app.use (function(request, response) { 
+    response.writeHead(200, { "Content-Type": "text/plain" }); 
+    response.end("Looks like you ddin't find a static file.");
+});
+
+http.createServer(app).listen(3000);
